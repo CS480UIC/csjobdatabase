f9665e489149b0eb348fa4014d7e2764f3cf5fdd
Tanmay Sarin <38360183+tanmaysarin@users.noreply.github.com>
2020-08-02 08:27:50 +0530

    Add files via upload

--- CS480Demo/src/user/web/servlet/UserServletResults.java
+++ CS480Demo/src/user/web/servlet/UserServletResults.java
@@ -16,16 +16,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import user.dao.ApplicationsDao;
-import user.dao.ResultsDao;
 import user.dao.UserDao;
 import user.dao.companiesDao;
-import user.domain.Applications;
 import user.domain.Results;
 import user.domain.User;
 import user.domain.companies;
+import user.service.ResultsService;
 import user.service.UserException;
 import user.service.UserService;
+import user.service.companiesService;
 
 /**
  * Servlet implementation class UserServlet
@@ -34,102 +33,45 @@ import user.service.UserService;
 public class UserServletResults extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	private ResultsDao resultDAO;
-	 
-    public void init() {
-        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
-        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
-        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+	public UserServletResults() {
+		super();
+	}
  
-        resultDAO = new ResultsDao(jdbcURL, jdbcUsername, jdbcPassword);
- 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
     }
  
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        doGet(request, response);
+    	ResultsService resultservice = new ResultsService();  
+		Results form = new Results();	
+
+		String application_link = request.getParameter("application_link");
+        int results_id = Integer.parseInt(request.getParameter("results_id"));
+        int application_id = Integer.parseInt(request.getParameter("application_id"));
+        String interview_progress = request.getParameter("interview_progress");
+        
+		form.setApplicationId(application_id);
+		form.setResultsId(results_id);
+		form.setApplicationLink(application_link);
+		form.setInterviewProgress(interview_progress);
+		
+		try {
+			resultservice.registResults(form);
+			request.getRequestDispatcher("/jsps/Results/Results_item.jsp").forward(request, response);
+			
+		} catch (ClassNotFoundException | UserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
- 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getServletPath();
- 
-        try {
-            switch (action) {
-            case "/new":
-                showNewForm(request, response);
-                break;
-            case "/insert":
-                insertResults(request, response);
-                break;
-            case "/delete":
-                deleteResults(request, response);
-                break;
-            case "/edit":
-                showEditForm(request, response);
-                break;
-            case "/update":
-                updateResults(request, response);
-                break;
-            default:
-                listResults(request, response);
-                break;
-            }
-        } catch (SQLException ex) {
-            throw new ServletException(ex);
-        }
-    }
- 
-    private void listResults(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException, ServletException {
-        List<Results> listResult = resultDAO.listAllResults();
-        request.setAttribute("listResult", listResult);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("ResultsList.jsp");
-        dispatcher.forward(request, response);
-    }
- 
-    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Results.jsp");
-        dispatcher.forward(request, response);
-    }
- 
-    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        Results existingBook = resultDAO.getResult(id);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Results.jsp");
-        request.setAttribute("Result", existingBook);
-        dispatcher.forward(request, response);
- 
-    }
- 
-    private void insertResults(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-        String interviewCall = request.getParameter("interview_call");
- 
-        Results newResult = new Results(interviewCall);
-        resultDAO.insertResults(newResult);
-        response.sendRedirect("list");
-    }
- 
-    private void updateResults(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-    	String interviewCall = request.getParameter("interview_call");
-    	 
-        Results newResult = new Results(interviewCall);
-        resultDAO.updateResults(newResult);
-        response.sendRedirect("list");
-    }
- 
-    private void deleteResults(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
- 
-        Results result= new Results(id);
-        resultDAO.deleteResults(result);
-        response.sendRedirect("list");
- 
-    }
+        
 
 }
