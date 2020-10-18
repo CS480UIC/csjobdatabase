fc1916ee39b2f1f75434800079ab510edd1f13d1
Tanmay Sarin <tanmaysarin@gmail.com>
2020-10-18 21:17:31 +0530

    Added new menu option

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -4,7 +4,9 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -13,15 +15,20 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -32,16 +39,17 @@ import javafx.util.Duration;
 
 public class JavaFXTemplate extends Application {
 	
-	private Button startButton, b1;
+	private Button startButton, b1, next, reset;
 	private TextField t1, text;
 	private MenuBar menu;
 	private EventHandler<ActionEvent> myHandler;
 	
-	int iterations = 0, numPressed = 0;;
+	int iterations = 0, numPressed = 0, drawingLoops = 0;
+	String temp = "";
 	
 	HashMap<String, Scene> sceneMap;
 	PauseTransition pause = new PauseTransition(Duration.seconds(1));
-	EventHandler<ActionEvent> returnButtons, submitAction, randomHandler;
+	EventHandler<ActionEvent> returnButtons, submitAction, randomHandler, drawingsHandler, checker, finalNumEvent;
 	
 	GenericQueue<String> myQueue;
 	//ListView<String> displayQueueItems;
@@ -61,7 +69,10 @@ public class JavaFXTemplate extends Application {
 		sceneMap = new HashMap<String,Scene>();
 		//sceneMap.put("WelcomeScene", createControlScene());
 		sceneMap.put("GameScene", createControlScene());
-		//sceneMap.put("ResultsScene", resultsControlScene());
+		sceneMap.put("ResultsScene", resultsControlScene());
+		
+		next.setOnAction( e->primaryStage.setScene(sceneMap.get("ResultsScene")));
+		//reset.setOnAction(e->primaryStage.setScene(sceneMap.get("GameScene")));
 		
 		Image pic = new Image("StartupImage.png");
 		ImageView v = new ImageView(pic);
@@ -84,26 +95,43 @@ public class JavaFXTemplate extends Application {
 		MenuItem iExit = new MenuItem("Exit");
 		
 		iExit.setOnAction(e -> Platform.exit());
+ 
+		Dialog<String> dialogRules = new Dialog<String>();
+        //Setting the title
+        dialogRules.setTitle("Rules");
+        ButtonType typeRules = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogRules.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogRules.getDialogPane().getButtonTypes().add(typeRules);
 		
-		//Creating a Text object 
-	    text = new TextField();
-	    text.setText("Hi!  Testing!!!!!");
-	    text.setEditable(false);
-	    text.setVisible(false);
-	      
-	    //Setting the text to be added. 
-	    //text.setText("Hello how are you"); 
-	    iRules.setOnAction(e -> text.setVisible(true));
+		iRules.setOnAction(e -> {
+			dialogRules.showAndWait();
+	    });
+		
+		Dialog<String> dialogOdds = new Dialog<String>();
+        //Setting the title
+        dialogOdds.setTitle("Odds of Winning");
+        ButtonType typeOdds = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogOdds.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogOdds.getDialogPane().getButtonTypes().add(typeOdds);
+		
+		iWinning.setOnAction(e -> {
+			dialogOdds.showAndWait();
+	    });
 		
 		// Add menu items to menu option
 		// Add menus to menu tab
+		
 		mMenu.getItems().addAll(iRules, iWinning, iExit);
 		menu.getMenus().add(mMenu);
-
+		
 /////////////////////////////////////////////////////////////////////////////////////
 		
 		StackPane stackPane = new StackPane();
-		stackPane.getChildren().addAll(new VBox(startButton), text);
+		stackPane.getChildren().addAll(new VBox(startButton));
 		// Set up the scene
 		Scene scene = new Scene(new VBox(menu, stackPane), 700, 700);
 //		Scene scene = new Scene(startButton, 700, 700);
@@ -118,15 +146,17 @@ public class JavaFXTemplate extends Application {
 	// method to create our first scene with controls
 	public Scene createControlScene() {
 		
+		//BorderPane pane = new BorderPane();
+		
 		Button random = new Button();
         random.setText("Random");
         random.setDisable(true);
         
-        Button reset = new Button();
+        reset = new Button();
         reset.setText("Reset");
         reset.setDisable(true);
         
-        Button next = new Button();
+        next = new Button();
         next.setText("Next");
         next.setDisable(true);
 		
@@ -139,29 +169,45 @@ public class JavaFXTemplate extends Application {
 		// Menu items
 		MenuItem iRules = new MenuItem("Rules");
 		MenuItem iWinning = new MenuItem("Odds of Winning");
+		MenuItem iLook = new MenuItem("New Look");
 		MenuItem iExit = new MenuItem("Exit");
 		
 		iExit.setOnAction(e -> Platform.exit());
 		
-		//Creating a Text object 
-	    text = new TextField();
-	    text.setText("Hi!  Testing!!!!!");
-	    text.setEditable(false);
-	    text.setVisible(false);
-	      
-	    //Setting the text to be added. 
-	    //text.setText("Hello how are you"); 
-	    iRules.setOnAction(e -> text.setVisible(true));
+		//iLook.setOnAction(e-> pane.setStyle("-fx-background-color: lightBlue;"));
+		
+		Dialog<String> dialogRules = new Dialog<String>();
+        //Setting the title
+        dialogRules.setTitle("Rules");
+        ButtonType typeRules = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogRules.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogRules.getDialogPane().getButtonTypes().add(typeRules);
+		
+		iRules.setOnAction(e -> {
+			dialogRules.showAndWait();
+	    });
+		
+		Dialog<String> dialogOdds = new Dialog<String>();
+        //Setting the title
+        dialogOdds.setTitle("Odds of Winning");
+        ButtonType typeOdds = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogOdds.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogOdds.getDialogPane().getButtonTypes().add(typeOdds);
+		
+		iWinning.setOnAction(e -> {
+			dialogOdds.showAndWait();
+	    });
 		
 		// Add menu items to menu option
 		// Add menus to menu tab
-		mMenu.getItems().addAll(iRules, iWinning, iExit);
+		mMenu.getItems().addAll(iRules, iWinning, iLook, iExit);
 		menu.getMenus().add(mMenu);
 		
 ///////////// Adding a GridPane and spots toggle in a HBox /////////////////////////////////////////		
-		
-//		GridPane grid = new GridPane();
-//		addGrid(grid); //populate the GridPane with buttons
 		
 		storeQueueItemsInListView = FXCollections.observableArrayList();
 		myQueue = new GenericQueue<String>("The selected numbers are:");
@@ -222,20 +268,6 @@ public class JavaFXTemplate extends Application {
         button2.setToggleGroup(spotsGroup);  
         button3.setToggleGroup(spotsGroup);  
         button4.setToggleGroup(spotsGroup);
-        
-	///////////////////////////////////////////////////
-//        Text text2 = new Text();
-//        text2.setText("Select the number of drawings");
-//        
-//        ToggleGroup drawingsGroup = new ToggleGroup();  
-//        RadioButton button5 = new RadioButton("1 drawing");  
-//        RadioButton button6 = new RadioButton("2 drawings");  
-//        RadioButton button7 = new RadioButton("3 drawings");  
-//        RadioButton button8 = new RadioButton("4 drawings");  
-//        button5.setToggleGroup(drawingsGroup);  
-//        button6.setToggleGroup(drawingsGroup);  
-//        button7.setToggleGroup(drawingsGroup);  
-//        button8.setToggleGroup(drawingsGroup);
         
     /////////////////////////////////////////////////
         Button submitButton = new Button("Submit Changes!");
@@ -316,10 +348,8 @@ public class JavaFXTemplate extends Application {
 		};
         
         random.setOnAction(randomHandler);
-        
-        
-        //reset.setOnAction();
-        //next.setOnAction(e->Scene.setScene(sceneMap.get("GameScene")));
+                
+//        reset.setOnAction(e->primaryStage.setScene(sceneMap.get("GameScene")));
         
 /////////////// Setup GridPane View /////////////////////////////////////////////////
 		
@@ -337,6 +367,9 @@ public class JavaFXTemplate extends Application {
         
         main.getChildren().addAll(menu,first,second,third);
 		main.setSpacing(20);
+		
+		//pane.setCenter(main);
+		iLook.setOnAction(e-> main.setStyle("-fx-background-color: lightBlue;"));		
 ////////////////////////////////////////////////////////////////////////////////////
 		
 		return new Scene(main, 700,700);
@@ -363,11 +396,160 @@ public class JavaFXTemplate extends Application {
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 	
-//	public Scene resultsControlScene() {
-//		
-//		Text text2 = new Text();      
-//        text2.setText("RESULTS!");
-//		return new Scene(b1,700,700);
-//	}
+	public Scene resultsControlScene() {
+		
+//////////////Adding A Menu at the TOP////////////////////////////////////////////
+		
+	// Add a menu with 1 tab and a few options
+		menu = new MenuBar(); //a menu bar takes menus as children
+		Menu mMenu = new Menu("Menu"); //a menu goes inside a menu bar
+		
+		// Menu items
+		MenuItem iRules = new MenuItem("Rules");
+		MenuItem iWinning = new MenuItem("Odds of Winning");
+		MenuItem iLook = new MenuItem("New Look");
+		MenuItem iExit = new MenuItem("Exit");
+		
+		iExit.setOnAction(e -> Platform.exit());
+		
+		Dialog<String> dialogRules = new Dialog<String>();
+        //Setting the title
+        dialogRules.setTitle("Rules");
+        ButtonType typeRules = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogRules.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogRules.getDialogPane().getButtonTypes().add(typeRules);
+		
+		iRules.setOnAction(e -> {
+			dialogRules.showAndWait();
+	    });
+		
+		Dialog<String> dialogOdds = new Dialog<String>();
+        //Setting the title
+        dialogOdds.setTitle("Odds of Winning");
+        ButtonType typeOdds = new ButtonType("Ok", ButtonData.OK_DONE);
+        //Setting the content of the dialog
+        dialogOdds.setContentText("This is a sample dialog");
+        //Adding buttons to the dialog pane
+        dialogOdds.getDialogPane().getButtonTypes().add(typeOdds);
+		
+		iWinning.setOnAction(e -> {
+			dialogOdds.showAndWait();
+	    });
+		
+		// Add menu items to menu option
+		// Add menus to menu tab
+		mMenu.getItems().addAll(iRules, iWinning, iLook, iExit);
+		menu.getMenus().add(mMenu);
+		
+		/////////////////////////////////////////////////////////////////
+		
+		Text drawingsText = new Text();      
+        drawingsText.setText("Select the number of drawings");
+        
+	    ToggleGroup drawingsGroup = new ToggleGroup();  
+	    RadioButton button5 = new RadioButton("1 drawing");  
+	    RadioButton button6 = new RadioButton("2 drawings");  
+	    RadioButton button7 = new RadioButton("3 drawings");  
+	    RadioButton button8 = new RadioButton("4 drawings");  
+	    button5.setToggleGroup(drawingsGroup);  
+	    button6.setToggleGroup(drawingsGroup);  
+	    button7.setToggleGroup(drawingsGroup);  
+	    button8.setToggleGroup(drawingsGroup);
+		
+	    HBox drawings = new HBox();
+	    drawings.getChildren().addAll(button5,button6,button7,button8);
+	    drawings.setSpacing(10);
+	    
+	    Button drawingsSubmit = new Button();
+	    drawingsHandler = new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				
+				drawingsSubmit.setDisable(true);
+				
+				drawingsGroup.getToggles().forEach(toggle -> { // Disable spotsGroup
+				    RadioButton node = (RadioButton) toggle ;
+				    node.setDisable(true);
+				});
+				
+				drawingsSubmit.setDisable(true);
+				
+				RadioButton selectedDrawingsButton = (RadioButton) drawingsGroup.getSelectedToggle();
+				String toogleGroupValue = selectedDrawingsButton.getText();
+				
+				if(toogleGroupValue == "1 drawing") {
+					drawingLoops = 1;
+				}else if(toogleGroupValue == "2 drawings") {
+					drawingLoops = 2;
+				}else if(toogleGroupValue == "3 drawings") {
+					drawingLoops = 3;
+				}else if(toogleGroupValue == "4 drawings") {
+					drawingLoops = 4;
+				}
+				System.out.println(drawingLoops);
+				
+				
+			}	
+        };
+        drawingsSubmit.setText("Submit");
+        drawingsSubmit.setOnAction(drawingsHandler);
+        
+        Button checkDraws = new Button("Check Draws!");
+        
+        TextArea drawNumbers = new TextArea();
+
+        checker = new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Random randNum = new Random();
+				Set<Integer>set = new LinkedHashSet<Integer>();
+				int num = randNum.nextInt(80)+1;
+				
+				if(set.contains(num) == false) {
+					set.add(num);
+					temp = temp + String.valueOf(num) + "  ";
+					
+				}
+				
+				drawNumbers.setText(temp);
+				
+			}        	
+        };
+        
+        Timeline timeline = new Timeline(
+        		new KeyFrame(Duration.seconds(1), checker),
+        		new KeyFrame(Duration.millis(1000))
+        );
+        
+        
+       	timeline.setCycleCount(20);
+       	
+        	
+        checkDraws.setOnAction(e -> {
+        	timeline.play();
+        });
+        
+        
+        Button nextDraw = new Button("Next Draw");
+        nextDraw.setOnAction(e -> {
+        	timeline.play();
+        	drawNumbers.setText(temp + "\n");
+        });
+        //nextDraw.setDisable(true);
+
+        
+		VBox topDown = new VBox();
+		topDown.getChildren().addAll(menu,drawingsText,drawings,drawingsSubmit,checkDraws,drawNumbers,nextDraw);
+		topDown.setSpacing(10);
+		
+		iLook.setOnAction(e-> topDown.setStyle("-fx-background-color: lightGreen;"));
+		return new Scene(topDown, 700,700);
+	}
 
 }
